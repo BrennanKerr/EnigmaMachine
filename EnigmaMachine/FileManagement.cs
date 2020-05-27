f69c27b7663a2fde0aacd3ed51deacb14a76e9d8
BrennanKerr <BrennanKerr@users.noreply.github.com>
2020-05-27 15:02:20 -0400

    Initial Files and FileManagement class

---
+++ EnigmaMachine/FileManagement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaMachine
+{
+    /// <summary>
+    /// The files that are stored in FILE_PATH array
+    /// </summary>
+    public enum FileNames
+    {
+        Rotors,
+        Reflectors
+    }
+
+    /// <summary>
+    /// Manages the file informaiton
+    /// </summary>
+    public static class FileManagement
+    {
+        /// <summary>
+        /// Stores the path for each file (ordered the same as FileNames)
+        /// </summary>
+        private static readonly string[] FILE_PATH =
+        {
+            "/files/rotors.txt",
+            "/files/reflectors.txt"
+        };
+
+        /// <summary>
+        /// Gets the corresponding path of the file name passed
+        /// </summary>
+        /// <param name="fn">the file name defined in FileNames</param>
+        /// <returns>the file path</returns>
+        public static string GetFilePath(FileNames fn)
+        {
+            string path = "";   // stores the path
+
+            // determines the file
+            if (fn == FileNames.Rotors) path = FILE_PATH[0];
+            else path = FILE_PATH[1];
+
+            return path;    // returns the file
+        }
+    }
+}
---
+++ EnigmaMachine/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace EnigmaMachine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            StreamReader reader = new StreamReader(Application.StartupPath + FileManagement.GetFilePath(FileNames.Rotors));
+            string input = "";
+
+            while (reader.Peek() > 0)
+            {
+                input += reader.ReadLine() + Environment.NewLine;
+            }
+
+            reader.Close();
+            MessageBox.Show(input);
+        }
+    }
+}
